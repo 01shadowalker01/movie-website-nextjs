a83d80df9c64dd103d44a553cc887f693423af87
Saeed Bayat <01shadowalker01@gmail.com>
2025-05-30 03:19:48 +0330

    feat: implement movie store

--- src/store/movieStore.ts
+++ src/store/movieStore.ts
@@ -1,7 +1,34 @@
 import { create } from "zustand";
 import { Movie } from "@/models/Movie";
 
-export const useStore = create((set) => ({
+type MovieStore = {
+  movies: Movie[];
+  moviesById: Record<string, Movie>;
+  getMovieById: (id: string) => Movie | undefined;
+  setMovies: (movies: Movie[]) => void;
+};
+
+export const movieStore = create<MovieStore>((set, get) => ({
   movies: [],
-  setMovies: (newMovies: Movie[]) => set({ bears: newMovies }),
+  moviesById: {},
+  getMovieById: (id) => get().moviesById[id],
+  setMovies: (movies) =>
+    set(() => {
+      const moviesById = convertToMoviesById(movies);
+
+      return {
+        movies,
+        moviesById,
+      };
+    }),
 }));
+
+function convertToMoviesById(movies: Movie[]): Record<string, Movie> {
+  return movies.reduce(
+    (acc, movie) => {
+      acc[movie.id] = movie;
+      return acc;
+    },
+    {} as Record<string, Movie>,
+  );
+}
